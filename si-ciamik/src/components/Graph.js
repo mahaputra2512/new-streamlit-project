4374fdca3fe5c58de8ea4abac3de9e029f0c61a4
Ihsan Fadli Tampati <ifat.ichan@gmail.com>
2024-01-31 10:24:51 +0700

    solving graph problems

--- si-ciamik/src/components/Graph.js
+++ si-ciamik/src/components/Graph.js
@@ -1,34 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Bar } from 'react-chartjs-2';
 
 const Graph = ({ data, semester }) => {
-  if (!data || !data.length) return null; // Tambahkan kondisi untuk menangani kasus data belum dimuat
+  const [chartData, setChartData] = useState({});
 
-  const graphData = {
-    labels: data.map((item) => item.nama),
-    datasets: [
-      {
-        label: `IPS Semester ${semester}`,
-        data: data.map((item) => item[semester]),
-        backgroundColor: 'rgba(75,192,192,0.2)',
-        borderColor: 'rgba(75,192,192,1)',
-        borderWidth: 1,
-      },
-    ],
-  };
+  useEffect(() => {
+    if (data && data.length) {
+      const labels = data.map((item) => item.nama);
 
-  const options = {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-  };
+      setChartData({
+        labels: labels,
+        datasets: [
+          {
+            label: `IPS Semester ${semester}`,
+            data: data.map((item) => item[semester]),
+            backgroundColor: 'rgba(75,192,192,0.2)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderWidth: 1,
+          },
+        ],
+      });
+    }
+  }, [data, semester]);
 
   return (
     <div>
       <h3>Grafik Indeks Prestasi Semester (IPS)</h3>
-      <Bar data={graphData} options={options} />
+      <Bar data={chartData} />
     </div>
   );
 };
